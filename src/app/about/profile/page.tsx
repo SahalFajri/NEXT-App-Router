0c010ac7c7e1e744c6064b444e9376cbe52ba297
SahalFajri <sahalfajri879@gmail.com>
2025-03-24 04:49:15 +0700

    2.Layout & Template

--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./navbar";
+import Navbar from "../../navbar";
 
 export default function ProfilePage() {
   return (
     <div>
-      <Navbar></Navbar>
+      Profile Page
     </div>
   )
 }
