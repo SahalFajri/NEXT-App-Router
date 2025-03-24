4d5e118c77071ad240d4b79da750fe105cda7088
SahalFajri <sahalfajri879@gmail.com>
2025-03-24 21:23:04 +0700

    11 Intercepting Route

---
+++ src/app/product/@modal/(.)detail/[id]/page.tsx
@@ -0,0 +1,16 @@
+import Modal from "@/components/core/Modal";
+import { getData } from "@/services/products";
+
+export default async function DetailProductPage(props: any) {
+  const { params } = props;
+  const { data } = await getData("http://localhost:3000/api/product/?id=" + params.id);
+  return (
+    <Modal>
+      <img src={data.image} alt="" className="w-full object-cover aspect-square col-span-2" />
+      <div className="bg-white p-4 px-6">
+        <h3>{data.title}</h3>
+        <p>Price: ${data.price}</p>
+      </div>
+    </Modal>
+  )
+}
