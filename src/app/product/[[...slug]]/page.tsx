dd3eaacb6b40096cc7189fe84bc4b2ab36e38603
SahalFajri <sahalfajri879@gmail.com>
2025-03-24 16:56:45 +0700

    8.Loading UI & Error Handling

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,9 +1,11 @@
 type ProductPageProps = { params: { slug: string[] } };
 
 async function getData() {
-  // const res = await fetch('https://fakestoreapi.com/products');
-  const res = await fetch('http://localhost:3000/api/product', {
-    cache: 'force-cache',
+  // const res = await fetch('https://fakestoreapi.com/products', {
+  //   cache: 'no-store',
+  // });
+  const res = await fetch('http://localhost:3000/api/productss', {
+    cache: 'no-store',
     next: {
       tags: ["products"],
       // revalidate: 10,
