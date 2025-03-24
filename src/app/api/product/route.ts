7e913d034587680105436cc504d9e5258fe1560c
SahalFajri <sahalfajri879@gmail.com>
2025-03-24 16:42:06 +0700

    7.Caching & Revalidating

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,15 +3,27 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
   {
     id: 1,
-    title: "Sepatu Baru",
-    price: 100000,
-    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/1983f2e1-0271-479c-bada-6176a571fa4f/NIKE+VOMERO+18.png",
+    title: "Nike Air Max Dn8",
+    price: 3049000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/59ff827c-5672-4657-9eca-0179dfe206c3/AIR+MAX+DN8.png",
   },
   {
     id: 2,
-    title: "Sepatu Baru Dong",
-    price: 10000000,
-    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/1983f2e1-0271-479c-bada-6176a571fa4f/NIKE+VOMERO+18.png",
+    title: "Nike Pegasus Premium",
+    price: 3299000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/81711d75-ce61-4233-b76e-a133d7a0cae8/NIKE+PEGASUS+PREMIUM.png",
+  },
+  {
+    id: 3,
+    title: "Nike Vomero 18",
+    price: 2249000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/88c35e4e-9a89-4ced-8247-c194da41b173/NIKE+VOMERO+18.png",
+  },
+  {
+    id: 4,
+    title: "Nike Vomero 18",
+    price: 2249000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/88c35e4e-9a89-4ced-8247-c194da41b173/NIKE+VOMERO+18.png",
   },
 ];
 
